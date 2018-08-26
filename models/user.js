64449b59db35b6379325642f1b3c0cbef25679f1
kneric <brian.pradipta.a@gmail.com>
2018-08-26 12:43:16 +0700

    fixed validation msg

--- models/user.js
+++ models/user.js
@@ -17,7 +17,8 @@ const UserSchema = new Schema({
       validator: function (email){
         let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         return re.test(email)
-      }
+      },
+      message: 'please input a valid email!' 
     }
   },
   password: {
